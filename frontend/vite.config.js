896d04b4d97771435f77a3308a6c2e2e9c10c656
deepika@riversilica.com <deepika@riversilica.com>
2025-04-17 17:00:44 +0530

    Fix vite permission issue for Render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -40,7 +40,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/', // 👈 THIS LINE IS CRUCIAL
+  base: './', // 👈 THIS LINE IS CRUCIAL
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }],
