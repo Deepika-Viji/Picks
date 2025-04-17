df622c64a7ce1d6a3c0035521bc4f2321c981fe8
deepika@riversilica.com <deepika@riversilica.com>
2025-04-17 16:21:58 +0530

    Fix vite permission issue for Render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -20,17 +20,33 @@
 // })
 
 
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import path from 'path'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }],
+//   },
+//   server: {
+//     host: true, // crucial for Render's 0.0.0.0 scanning
+//     port: 5173, // optional; you can use Render's $PORT env variable too
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  base: '/', // 👈 THIS LINE IS CRUCIAL
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }],
   },
   server: {
-    host: true, // crucial for Render's 0.0.0.0 scanning
-    port: 5173, // optional; you can use Render's $PORT env variable too
-  }
+    host: true, // allow external access
+    port: 5173, // optional
+  },
 })
